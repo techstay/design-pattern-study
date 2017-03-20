18ca6149be0292eb4eb615aa961c1c2155474aba
yitian <techstay@aliyun.com>
2017-03-20 23:31:59 +0800

    添加了桥接模式的例子。

--- design-pattern-java-samples/src/main/java/yitian/study/Application.java
+++ design-pattern-java-samples/src/main/java/yitian/study/Application.java
@@ -3,6 +3,7 @@ package yitian.study;
 
 import yitian.study.abstractfactory.AbstractFactorySample;
 import yitian.study.adapter.AdapterSample;
+import yitian.study.bridge.BridgeSample;
 import yitian.study.builder.BuilderSample;
 import yitian.study.factory.FactorySample;
 import yitian.study.prototype.PrototypeSample;
@@ -29,5 +30,9 @@ public class Application {
         System.out.println("适配器模式");
         Sample adapterSample = new AdapterSample();
         adapterSample.run();
+
+        System.out.println("桥接模式");
+        Sample bridgeSample = new BridgeSample();
+        bridgeSample.run();
     }
 }
